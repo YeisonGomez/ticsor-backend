91373f4203f1ef663034dce6579da0bcc48d0d23
YeisonGomez <yeigomez97@hotmail.com>
2017-11-22 06:29:18 -0500

    init ticsor-backend

--- src/environment/index.ts
+++ src/environment/index.ts
@@ -11,6 +11,4 @@ export const CONFIG = {
 		password: process.env.DBPASSWORD || "persian13",
 		database : process.env.DBNAME || "hangers_chess"
 	},
-	//CODE_PRIVATE: "hangers_chess_hulk_cool_123_code_123_@34123123213756435324555&"
-	CODE_PRIVATE: "847473628"
 }
--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { BoxsModule } from './boxs/boxs.module';
 import { UserModule } from './users/users.module';
-import { ScoreModule } from './score/score.module';
 
 @Module({
-    modules: [BoxsModule, UserModule, ScoreModule],
+    modules: [UserModule],
 })
 export class ApplicationModule {}
